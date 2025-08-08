d975b61a079cb9fbab4418508ea52df14f11ed8e
Subin Paul <subin01@gmail.com>
2025-08-08 18:33:35 +1000

    sw path updates 5

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,9 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const API_CACHE_NAME = 'api-cache-v1';
 const urlsToCache = [
+  '/icon-192x192.png',
+  '/favicon.ico',
+  '/sw-register.js',
   '/_next/static/js/main-app.js',
   '/_next/static/js/app/layout.js',
   '/_next/static/js/app/todos/page.js',
